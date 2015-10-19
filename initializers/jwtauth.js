7ddbac814e58158f8de49c7531ac1b8467458f2d
Mario Micklisch <mario@favo.org>
2015-10-19 19:44:25 +0200

    compatibility with actionhero 11+

--- initializers/jwtauth.js
+++ initializers/jwtauth.js
@@ -1,25 +1,34 @@
 var jsonwebtoken = require ('jsonwebtoken');
-exports.jwtauth = function(api, next) {
-  api.jwtauth = {
-    processToken: function(token, success, fail) {
-      jsonwebtoken.verify(token, api.config.jwtauth.secret, {}, function(err, data) {
-        if(err) {
-          fail(err);
-        } else {
-          success(data);
-        }
-      });
+module.exports = {
+    loadPriority: 1000,
+    startPriority: 1000,
+    stopPriority: 1000,
+
+    start: function(api, next) {
+        api.jwtauth = {
+            processToken: function(token, success, fail) {
+                jsonwebtoken.verify(token, api.config.jwtauth.secret, {}, function(err, data) {
+                if(err) {
+                  fail(err);
+                } else {
+                  success(data);
+                }
+            });
+        },
+        generateToken: function(data, success, fail) {
+            try {
+                var token = jsonwebtoken.sign(data, api.config.jwtauth.secret, {
+                  algorithm: api.config.jwtauth.algorithm
+                });
+                success(token);
+                } catch(err) {
+                fail(err);
+                }
+            }
+        };
+        next();
     },
-    generateToken: function(data, success, fail) {
-      try {
-        var token = jsonwebtoken.sign(data, api.config.jwtauth.secret, {
-          algorithm: api.config.jwtauth.algorithm
-        });
-        success(token);
-      } catch(err) {
-        fail(err);
-      }
+    stop: function(api, next) {
+        next();
     }
-  };
-  next();
-};
+}
