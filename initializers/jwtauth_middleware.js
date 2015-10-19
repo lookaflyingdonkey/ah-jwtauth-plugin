a7c4f926ef692bacee33f9e713bc972a4e8ef541
Mario Micklisch <mario@favo.org>
2015-10-19 20:30:39 +0200

    added (working) authenticate attribute for actions

--- initializers/jwtauth_middleware.js
+++ initializers/jwtauth_middleware.js
@@ -1,56 +1,59 @@
-exports.jwtauth_middleware = function(api, next) {
-  var jwtAuthPreProcessor = function(connection, actionTemplate, next) {
-    if(actionTemplate.authenticated === true && api.config.jwtauth.enabled[connection.type] && api.config.jwtauth.enabled[connection.type] === true) {
-      var req = connection.rawConnection.req;
-      if(!req && connection.mockHeaders) {
-        req = {
-          headers: connection.mockHeaders
-        };
-      }
-      if(req.headers && req.headers['authorization']) {
-        var parts = req.headers['authorization'].split(' ');
-        if(parts.length != 2) {
-          connection.rawConnection.responseHttpCode = 500;
-          connection.error = {
-            code: 500,
-            message: 'Invalid Authorization Header'
-          };
-          next(connection, false);
-        } else {
-          if(parts[0].toLowerCase() != 'token') {
-            connection.rawConnection.responseHttpCode = 500;
-            connection.error = {
-              code: 500,
-              message: 'Invalid Authorization Header'
-            };
-            next(connection, false);
-          } else {
-            api.jwtauth.processToken(parts[1], function(data) {
-              // Valid data, lets set it and continue
-              connection.user = data;
-              next(connection, true);
-            }, function(err) {
-              connection.rawConnection.responseHttpCode = err.http_status;
-              delete err.http_status;
-              connection.error = err;
-              next(connection, false);
-            });
-          }
-        } 
-      } else {
-          connection.rawConnection.responseHttpCode = 500;
-          connection.error = {
-            code: 500,
-            message: 'Authorization Header Not Set'
-          };
-          next(connection, false);
-      }
-    } else {
-      next(connection, true);
+var jsonwebtoken = require ('jsonwebtoken');
+
+module.exports = {
+
+  initialize: function(api, next){
+
+    var jwtmiddleware = {
+        name: 'jwt token validator',
+        global: true,
+        preProcessor: function(data, next) {
+            if(data.actionTemplate.authenticate && api.config.jwtauth.enabled[data.connection.type] && api.config.jwtauth.enabled[data.connection.type] === true) {
+              var req = data.connection.rawConnection.req;
+              if(!req && data.mockHeaders) {
+                req = {
+                  headers: data.mockHeaders
+                };
+              }
+              if(req.headers && req.headers['authorization']) {
+                var parts = req.headers['authorization'].split(' ');
+                if(parts.length != 2) {
+                  next({
+                    code: 500,
+                    message: 'Invalid Authorization Header'
+                  });
+                } else {
+                  if(parts[0].toLowerCase() != 'token') {
+                    next({
+                      code: 500,
+                      message: 'Invalid Authorization Header'
+                    });
+                  } else {
+                    api.jwtauth.processToken(parts[1], function(tokenData) {
+                      data._jwtTokenData = tokenData;
+                      next();
+                    }, function(err) {
+                      next(err);
+                    });
+                  }
+                } 
+              } else {
+                  next({
+                    code: 500,
+                    message: 'Authorization Header Not Set'
+                  });
+              }
+            } else {
+              next();
+            }
+        },
+        stop: function(api, next) {
+            next();
+        }
     }
-  };
-  
-  api.actions.addPreProcessor(jwtAuthPreProcessor);
-  
-  next();
-}
+
+
+    api.actions.addMiddleware(jwtmiddleware);
+    next();
+  }
+};
